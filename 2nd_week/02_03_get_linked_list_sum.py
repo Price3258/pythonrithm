e07f6e114077df092d6f9b5b19c843be0fc4015a
김원태 <rladnjsxo123@naver.com>
2024-12-27 12:22:58 +0900

    Update 02_03_get_linked_list_sum.py

--- 2nd_week/02_03_get_linked_list_sum.py
+++ 2nd_week/02_03_get_linked_list_sum.py
@@ -1,3 +1,11 @@
+# Q.  다음과 같은 두 링크드 리스트를 입력받았을 때, 합산한 값을 반환하시오.
+#
+# 예를 들어 아래와 같은 링크드 리스트를 입력받았다면,
+# 각각 678, 354 이므로 두개의 총합
+# 678 + 354 = 1032 를 반환해야 한다.
+#
+# 단, 각 노드의 데이터는 한자리 수 숫자만 들어갈 수 있다.
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -38,4 +46,4 @@ linked_list_2 = LinkedList(3)
 linked_list_2.append(5)
 linked_list_2.append(4)
 
-print(get_linked_list_sum(linked_list_1, linked_list_2)) # 1032 가 되어야 함. 
+print(get_linked_list_sum(linked_list_1, linked_list_2)) # 1032 가 되어야 함.
