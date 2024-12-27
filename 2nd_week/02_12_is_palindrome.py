d838aa3b400848813ac72bdb6c27ca5fbdaa186c
김원태 <rladnjsxo123@naver.com>
2024-12-27 15:17:04 +0900

    Create 02_12_is_palindrome.py

---
+++ 2nd_week/02_12_is_palindrome.py
@@ -0,0 +1,24 @@
+
+
+def is_palindrome(string):
+    n = len(string)
+    for i in range(n):
+        if string[i] != string[n - i - 1]:
+            return False
+    return True
+
+input = "abcba"
+
+print(is_palindrome(input))
+
+
+def is_palindrome_v2(string):
+    if len(string) <= 1:
+        return True
+    if string[0] != string[-1]:
+        return False
+    return is_palindrome(string[1:-1])
+
+input = "abcba"
+
+print(is_palindrome_v2(input))
