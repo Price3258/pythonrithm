a75cc667659d19e6882e72af29bc34710fcd4e25
김원태 <rladnjsxo123@naver.com>
2024-12-27 17:40:34 +0900

    Update is_available_to_order.py

--- 2nd_week/is_available_to_order.py
+++ 2nd_week/is_available_to_order.py
@@ -27,3 +27,31 @@ def is_available_to_order(menus, orders):
 
 result = is_available_to_order(shop_menus, shop_orders)
 print(result)
+
+def is_available_to_order_v2(menus, orders):
+    menus.sort()  # menus 정렬!
+    for order in orders:
+        if not is_existing_target_number_binary(order, menus):
+            return False
+    return True
+
+
+def is_existing_target_number_binary(target, array):
+    current_min = 0
+    current_max = len(array) - 1
+    current_guess = (current_min + current_max) // 2
+
+    while current_min <= current_max:
+        if array[current_guess] == target:
+            return True
+        elif array[current_guess] < target:
+            current_min = current_guess + 1
+        else:
+            current_max = current_guess - 1
+        current_guess = (current_min + current_max) // 2
+
+    return False
+
+
+result = is_available_to_order_v2(shop_menus, shop_orders)
+print(result)
