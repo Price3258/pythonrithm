461bf15b52bd2588322ffc6bea8796c260232c4a
김원태 <rladnjsxo123@naver.com>
2025-01-06 13:08:10 +0900

    Create 04_06_fibo_recursion.py

---
+++ 4st_week/04_06_fibo_recursion.py
@@ -0,0 +1,13 @@
+
+
+def fibo_recursion(n):
+
+    if n ==1 or n==2:
+        return 1
+
+
+    return fibo_recursion(n-1) + fibo_recursion(n-2)
+
+
+print(fibo_recursion(20))
+print(fibo_recursion(100))
