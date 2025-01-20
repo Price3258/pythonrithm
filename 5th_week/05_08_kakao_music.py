68fb01840f1d5fdd9799da3b184db1eec9378ac3
김원태 <rladnjsxo123@naver.com>
2025-01-20 21:22:50 +0900

    Update 05_08_kakao_music.py

--- 5th_week/05_08_kakao_music.py
+++ 5th_week/05_08_kakao_music.py
@@ -1,23 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17683
-
+import math
 
 """
 
 C, C#, D, D#, E, F, F#, G, G#, A, A#, B
 """
 def convert_sharps(m):
-    return m.replace("C#","c").replace("D#","d").replace("G#","g").replace("A#","a")
+    return m.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
+
+def get_play_time(start, end):
+    start_h, start_m = start.split(':')
+    end_h, end_m = end.split(':')
+
+    return (int(end_h) - int(start_h)) *60 + int(end_m) - int(start_m)
 
 def solution(m, musicinfos):
-    answer = ''
+    answer = None
+    music = convert_sharps(m)
+    max_play_time = 0
 
-    m = convert_sharps(m)
+    for info in musicinfos:
+        start_time, end_time, name, melody = info.split(",")
+        play_time = get_play_time(start_time, end_time)
 
-    print(m)
+        melody = convert_sharps(melody)
+        melody_repeated_count = math.ceil(play_time / len(melody))
+        melody_played = (melody * melody_repeated_count)[:play_time]
+        if music in melody_played and play_time > max_play_time:
+            answer = name
+            max_play_time = play_time
 
-
-
-    return '(None)'
+    if not answer:
+        return "(None)"
+    return answer
 
 m = "ABCDEFG"
 music_info = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
